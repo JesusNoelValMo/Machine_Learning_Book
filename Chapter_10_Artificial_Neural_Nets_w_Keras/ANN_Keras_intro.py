9309a9afe53f12922261e8681d3d44d737b3d077
Jesus Noel Valdibiezo Mora <jesusnoel.valdibiezomora@outlook.com>
2021-04-10 18:56:37 -0600

    adding ANN_Keras_intro and image_classigier_with_sequential_api docs

---
+++ Chapter_10_Artificial_Neural_Nets_w_Keras/ANN_Keras_intro.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb  8 20:59:00 2021
+
+@author: jesus
+"""
+import tensorflow as tf 
+from tensorflow import keras
+print(tf.__version__, keras.__version__)
